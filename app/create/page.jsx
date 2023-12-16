2a8bf6adfc5badd374cb0612255fb50257f89813
ly-schneider <lyschneider@icloud.com>
2023-12-16 22:52:37 +0100

    Fixed so that the until date actually works

--- app/create/page.jsx
+++ app/create/page.jsx
@@ -26,6 +26,7 @@ export default function CreatePostPage() {
   const [errorImage, setErrorImage] = useState("");
 
   const [banned, setBanned] = useState(false);
+  const [banCreating, setBanCreating] = useState(false);
   const [banData, setBanData] = useState([]);
 
   useEffect(() => {
@@ -44,6 +45,9 @@ export default function CreatePostPage() {
                 console.log(ban);
                 if (ban.type == "account") {
                   setBanned(true);
+                  setBanData(ban);
+                } else if (ban.type == "create") {
+                  setBanCreating(true);
                   setBanData(ban);
                 }
               });
@@ -145,113 +149,137 @@ export default function CreatePostPage() {
           </p>
         </div>
       ) : (
-        <div className="mx-12 sm:mx-20 mt-8">
-          <div className="flex items-center">
-            <img
-              src={profile.profileimage}
-              className="w-16 h-16 rounded-full me-4 object-cover border-[3px] border-accent"
-            />
-            <h1 className="title font-bold">{profile.username}</h1>
-          </div>
-          <form onSubmit={(e) => handleSubmit(e)} className="mt-8">
-            <div>
-              <label
-                className={
-                  "text text-sm ms-1.5 px-3 py-1 rounded-t-form" +
-                  (errorTitle != "" ? " bg-error opacity-50" : " bg-primary ")
-                }
-                htmlFor="titel"
-              >
-                Titel
-              </label>
-              {errorTitle != "" && (
-                <div className="bg-error font-bold rounded-t-div px-3 py-2 text-text text text-sm">
-                  {errorTitle}
-                </div>
-              )}
-              <input
-                className={
-                  "input w-full" +
-                  (errorTitle != ""
-                    ? " border-error rounded-b-form rounded-t-none"
-                    : "")
-                }
-                type="text"
-                id="titel"
-                autoComplete="off"
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-              />
+        <>
+          {banCreating ? (
+            <div className="flex flex-col items-center w-full mt-8">
+              <h1 className="title font-extrabold text-error">
+                Du bist für das Posten gesperrt!
+              </h1>
+              <h1 className="title text-lg font-extrabold text-error">
+                Bis: {calcTime(banData.until)}
+              </h1>
+              <p className="text text-base text-center mt-3">
+                Grund dafür ist: {banData.reason}
+              </p>
+              <p className="text text-sm text-muted text-center mt-3">
+                Du wurdest von {banData.bannedby} am{" "}
+                {calcTime(banData.createdat)} gebannt.
+              </p>
             </div>
-            <div className="mt-5">
-              <label
-                className={
-                  "text text-sm ms-1.5 px-3 py-1 rounded-t-form" +
-                  (errorText != "" ? " bg-error opacity-50" : " bg-primary ")
-                }
-                htmlFor="text"
-              >
-                Text
-              </label>
-              {errorText != "" && (
-                <div className="bg-error font-bold rounded-t-div px-3 py-2 text-text text text-sm">
-                  {errorText}
-                </div>
-              )}
-              <textarea
-                className={
-                  "min-h-[45px] input w-full" +
-                  (errorText != ""
-                    ? " border-error rounded-b-form rounded-t-none"
-                    : "")
-                }
-                rows={5}
-                id="text"
-                type="text"
-                autoComplete="off"
-                value={text}
-                onChange={(e) => setText(e.target.value)}
-              />
-            </div>
-            <div className="flex justify-between mt-5">
-              <div className="flex flex-row items-center">
-                <label
-                  htmlFor="fileInput"
-                  className="inline-block cursor-pointer px-5 py-2 btn-secondary text-sm"
-                >
-                  <FontAwesomeIcon
-                    icon={faPlusSquare}
-                    className="h-3 w-3 me-1 mb-[1px]"
-                  />
-                  {file ? "Bild ändern" : "Bild hinzufügen"}
+          ) : (
+            <div className="mx-12 sm:mx-20 mt-8">
+              <div className="flex items-center">
+                <img
+                  src={profile.profileimage}
+                  className="w-16 h-16 rounded-full me-4 object-cover border-[3px] border-accent"
+                />
+                <h1 className="title font-bold">{profile.username}</h1>
+              </div>
+              <form onSubmit={(e) => handleSubmit(e)} className="mt-8">
+                <div>
+                  <label
+                    className={
+                      "text text-sm ms-1.5 px-3 py-1 rounded-t-form" +
+                      (errorTitle != ""
+                        ? " bg-error opacity-50"
+                        : " bg-primary ")
+                    }
+                    htmlFor="titel"
+                  >
+                    Titel
+                  </label>
+                  {errorTitle != "" && (
+                    <div className="bg-error font-bold rounded-t-div px-3 py-2 text-text text text-sm">
+                      {errorTitle}
+                    </div>
+                  )}
                   <input
-                    type="file"
-                    id="fileInput"
-                    onChange={handleFileChange}
-                    style={{ display: "none" }}
+                    className={
+                      "input w-full" +
+                      (errorTitle != ""
+                        ? " border-error rounded-b-form rounded-t-none"
+                        : "")
+                    }
+                    type="text"
+                    id="titel"
+                    autoComplete="off"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
                   />
-                </label>
-              </div>
-              <button className="btn-primary text text-sm" type="submit">
-                Post
-              </button>
+                </div>
+                <div className="mt-5">
+                  <label
+                    className={
+                      "text text-sm ms-1.5 px-3 py-1 rounded-t-form" +
+                      (errorText != ""
+                        ? " bg-error opacity-50"
+                        : " bg-primary ")
+                    }
+                    htmlFor="text"
+                  >
+                    Text
+                  </label>
+                  {errorText != "" && (
+                    <div className="bg-error font-bold rounded-t-div px-3 py-2 text-text text text-sm">
+                      {errorText}
+                    </div>
+                  )}
+                  <textarea
+                    className={
+                      "min-h-[45px] input w-full" +
+                      (errorText != ""
+                        ? " border-error rounded-b-form rounded-t-none"
+                        : "")
+                    }
+                    rows={5}
+                    id="text"
+                    type="text"
+                    autoComplete="off"
+                    value={text}
+                    onChange={(e) => setText(e.target.value)}
+                  />
+                </div>
+                <div className="flex justify-between mt-5">
+                  <div className="flex flex-row items-center">
+                    <label
+                      htmlFor="fileInput"
+                      className="inline-block cursor-pointer px-5 py-2 btn-secondary text-sm"
+                    >
+                      <FontAwesomeIcon
+                        icon={faPlusSquare}
+                        className="h-3 w-3 me-1 mb-[1px]"
+                      />
+                      {file ? "Bild ändern" : "Bild hinzufügen"}
+                      <input
+                        type="file"
+                        id="fileInput"
+                        onChange={handleFileChange}
+                        style={{ display: "none" }}
+                      />
+                    </label>
+                  </div>
+                  <button className="btn-primary text text-sm" type="submit">
+                    Post
+                  </button>
+                </div>
+                <p className="mt-2 text-xs text" id="fileName">
+                  {file ? file.name : ""}
+                </p>
+                {errorImage != "" && (
+                  <div className="bg-error font-bold mt-5 rounded-div px-3 py-2 text-text text text-sm">
+                    {errorImage}
+                  </div>
+                )}
+                <div className="mt-5">
+                  <img
+                    className="rounded-image"
+                    src={file ? URL.createObjectURL(file) : ""}
+                  />
+                </div>
+              </form>
             </div>
-            <p className="mt-2 text-xs text" id="fileName">
-              {file ? file.name : ""}
-            </p>
-            {errorImage != "" && (
-              <div className="bg-error font-bold mt-5 rounded-div px-3 py-2 text-text text text-sm">
-                {errorImage}
-              </div>
-            )}
-            <div className="mt-5">
-              <img
-                className="rounded-image"
-                src={file ? URL.createObjectURL(file) : ""}
-              />
-            </div>
-          </form>
-        </div>
+          )}
+        </>
       )}
     </>
   );
--- components/auth/checkBan.js
+++ components/auth/checkBan.js
@@ -1,10 +1,39 @@
 import supabase from "../supabase";
 
 export async function checkBan(accountId) {
+  function toIsoString(date) {
+    var tzo = -date.getTimezoneOffset(),
+      dif = tzo >= 0 ? "+" : "-",
+      pad = function (num) {
+        return (num < 10 ? "0" : "") + num;
+      };
+
+    return (
+      date.getFullYear() +
+      "-" +
+      pad(date.getMonth() + 1) +
+      "-" +
+      pad(date.getDate()) +
+      "T" +
+      pad(date.getHours()) +
+      ":" +
+      pad(date.getMinutes()) +
+      ":" +
+      pad(date.getSeconds()) +
+      dif +
+      pad(Math.floor(Math.abs(tzo) / 60)) +
+      ":" +
+      pad(Math.abs(tzo) % 60)
+    );
+  }
+
+  const currentDate = toIsoString(new Date());
+
   const { data: banData, error: banError } = await supabase
     .from("banned")
     .select("id_ban, createdat, account_id, bannedby_id, type, reason, until")
-    .eq("account_id", accountId);
+    .eq("account_id", accountId)
+    .gt("until", currentDate);
 
   if (banError) {
     console.log(banError);
