e6f9c481c84cea5af0e7d8f603f87906c42488e2
lyschneider <lyschneider@icloud.com>
2024-01-03 15:29:37 +0100

    Added unfinished notifications button

---
+++ components/buttons/notificationButton.jsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { faBell } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Tooltip } from "flowbite-react";
+import { useRouter } from "next/navigation";
+
+export default function NotificationButton() {
+  const router = useRouter();
+
+  function handleClick() {
+    router.push("/create/");
+  }
+
+  return (
+    <>
+      <Tooltip
+        content="Mitteilung"
+        className="text-sm font-medium text-white bg-zinc-800 !mt-2"
+      >
+        <button
+          onClick={handleClick}
+          className="text-background py-1 px-[9px] sm:py-2 sm:px-[13px] rounded-full border-2 border-transparent hover:bg-background hover:text-text hover:border-text transition-all duration-500 bg-text"
+        >
+          <FontAwesomeIcon icon={faBell} />
+        </button>
+      </Tooltip>
+    </>
+  );
+}
